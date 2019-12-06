e5215979dd6132eae973b0addc10f2517a98ec68
Yann WUILLAUME <yann.wuillaume@gmail.com>
2019-12-06 10:39:56 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Import React and other component
+import React, { Component } from 'react'
+import marked from 'marked'
+//Import CSS
+import './App.css'
+//Import sampleText
+import { sampleText } from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //Déclaration du state du component contenant le sampleText importé 
+  state = {
+    text: sampleText
+  }
+
+  //Action au chargement du composant, est-il correctement "monté" ?
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+
+    text ? this.setState({ text }) :  this.setState({ text: sampleText })
+    
+  }
+
+  //Action à la mise à jour du composant
+  componentDidUpdate () {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  //Utilisation de marked dans la fonction pour retourner le texte formaté, on renvoie 
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+
+  //Action au changement, mise à jour du state pour prendre la nouvelle value de l'input (ici le text area)
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+   render() {
+    return(
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <textarea
+              onChange={this.handleChange}
+              className='form-control'
+              value={this.state.text}
+              rows='30'
+              ></textarea>
+          </div>
+          <div className='col-sm-6'>
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} ></div>
+          </div>
+        </div>
+      </div>
+    )    
+  }
 }
 
 export default App;
